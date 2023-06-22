c1c2903af5f123c663d04c55010939772ea85799
Bittu <bittukumarthakur@users.noreply.github.com>
2023-06-22 16:05:56 +0530

    Added transition in cards.

--- generate-cards/card-with-img.js
+++ generate-cards/card-with-img.js
@@ -9,12 +9,66 @@ const format = function (detail) {
   let lineBreak = "";
 
   if (number % 4 === 0) {
-    // lineBreak = "<br>";
     lineBreak = "<div/>\n<div>";
 
   }
 
   const threeDigitNum = number.padStart(3, 0)
+
+
+  return `
+  <section class="card">
+
+  <!-- photo -->
+  <div class="photo">
+  <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${threeDigitNum}.png" alt="img" width="210px" >
+  </div>
+
+  <!-- name -->
+  <div class="Pokemon-name">${name}</div>
+
+  <!-- details -->
+  <section>
+
+    <!-- Type -->
+    <div class="line">
+      <span class="title">Type</span>
+      <span class="value">${type}</span>
+    </div>
+
+    <!-- Weight -->
+    <div class="line">
+      <span class="title">Weight</span>
+      <span class="value">${weight}</span>
+    </div>
+
+    <!-- Hp -->
+    <div class="line">
+      <span class="title">Hp</span>
+      <span class="value">${hp}</span>
+    </div>
+
+    <!-- Xp -->
+    <div class="line">
+      <span class="title">Xp</span>
+      <span class="value">${xp}</span>
+    </div>
+
+    <!-- Attack -->
+    <div class="line">
+      <span class="title">Attack</span>
+      <span class="value">${attack}</span>
+    </div>
+
+    <!-- Defence -->
+    <div class="line">
+      <span class="title">Defence</span>
+      <span class="value">${defence}</span>
+    </div>
+
+  </section>
+</section>
+  `
 
   return `<!-- first card -->
       <div class="card">
@@ -28,7 +82,7 @@ const format = function (detail) {
           </div>
         </div>
 
-        <!-- name -->
+        <!-- name-${name} -->
         <div class="fake-table"></div>
         <div class="Pokemon-name">
           <div>${name}</div>
@@ -81,7 +135,7 @@ const format = function (detail) {
 
 const main = function () {
 
-  const data = fs.readFileSync("./data.txt", "utf-8");
+  const data = fs.readFileSync("generate-cards/raw-data.txt", "utf-8");
 
   const rawData = data.split("\n")
   const lines = rawData.slice(1)
@@ -90,12 +144,8 @@ const main = function () {
     return format(line.split("|"));
   });
 
-  // console.log(finalData)
   process.stdout.write(finalData.join(""));
-
-
 
 }
 
 main();
-// console.log(format(["4", "bulbasaur", "grass,poison", "45", "45", "64", "49", "49", "69"]))
